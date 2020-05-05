4d01ca73eb34f6c30b3b2b5e00bcfde7e864e2f4
Alex A.F <alex2andres@gmail.com>
2020-05-05 11:43:28 +0200

    Adds AddFilm Activity and FloatingButton action on Main

---
+++ app/src/main/java/com/vidalibarraquer/euf2_andres_alex/AddFilmActivity.java
@@ -0,0 +1,14 @@
+package com.vidalibarraquer.euf2_andres_alex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddFilmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_film);
+    }
+}
--- app/src/main/java/com/vidalibarraquer/euf2_andres_alex/MainActivity.java
+++ app/src/main/java/com/vidalibarraquer/euf2_andres_alex/MainActivity.java
@@ -2,13 +2,37 @@ package com.vidalibarraquer.euf2_andres_alex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    // Visual components assignation
+
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assign visual component to the variable
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        // Let's put a onClick Listener
+        floatingActionButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.floatingActionButton:
+                // Load AddFilm Activity
+                Intent intent = new Intent(this, AddFilmActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
